9225180a58e055f6221d4240e424976005eb0281
H4ch1k0 <hachisoft@hotmail.it>
2018-08-03 13:10:30 +0200

    Ability Selection Popup now appears after Overlord Selection

--- Assets/Scripts/App/Pages/HeroSelectionPage.cs
+++ Assets/Scripts/App/Pages/HeroSelectionPage.cs
@@ -144,10 +144,16 @@ namespace LoomNetwork.CZB
         {
             _soundManager.PlaySound(Enumerators.SoundType.CLICK, Constants.SFX_SOUND_VOLUME, false, false, true);
 
-            _uiManager.GetPage<DeckEditingPage>().CurrentHeroId = _currentOverlordObject.SelfHero.heroId;
-
-            _appStateManager.ChangeAppState(Enumerators.AppState.DECK_EDITING);
+			OverlordAbiltySelectionPopup.OnHidePopupEvent += AbilityPopupClosedEvent;
+			_uiManager.DrawPopup<OverlordAbiltySelectionPopup> (_currentOverlordObject.SelfHero);
         }
+
+		private void AbilityPopupClosedEvent()
+		{
+			OverlordAbiltySelectionPopup.OnHidePopupEvent -= AbilityPopupClosedEvent;
+			_uiManager.GetPage<DeckEditingPage>().CurrentHeroId = _currentOverlordObject.SelfHero.heroId;
+			_appStateManager.ChangeAppState(Enumerators.AppState.DECK_EDITING);
+		}
 
         private void LeftArrowButtonOnClickHandler()
         {
