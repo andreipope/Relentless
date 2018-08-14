21f175d9f1ed196571b633b6a0c59b05d5a96faa
Serhii Yolkin <serhii.yolkin@lostpolygon.com>
2018-08-14 20:09:32 +0200

    Always use remote decks if available

--- Assets/Scripts/App/Managers/DataManager.cs
+++ Assets/Scripts/App/Managers/DataManager.cs
@@ -345,7 +345,12 @@ namespace LoomNetwork.CZB
                                 localDecksData = DeserializeObjectFromPath<DecksData>(_cacheDataPathes[type]);
                         }
 
-                        if (localDecksData != null && remoteDecksData != null)
+
+                        // FIXME: remove when enabling deck sync
+                        CachedDecksData = remoteDecksData ?? localDecksData;
+
+                        // Uncomment to switch deck sync
+                        /*if (localDecksData != null && remoteDecksData != null)
                         {
                             if (remoteDecksDataTimestamp == localDecksDataTimestamp)
                             {
@@ -395,7 +400,7 @@ namespace LoomNetwork.CZB
                         {
                             Debug.Log("Using local decks data");
                             CachedDecksData = localDecksData;
-                        }
+                        }*/
                     }
                     break;
                 case Enumerators.CacheDataType.DECKS_OPPONENT_DATA:
