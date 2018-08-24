e65632a276987fb014ee3490838c84b88c9af833
Serhii Yolkin <serhii.yolkin@lostpolygon.com>
2018-08-24 19:23:42 +0200

    Automatic build id

--- Assets/Scripts/Build/Editor/BuildMetaInfoGenerator.cs
+++ Assets/Scripts/Build/Editor/BuildMetaInfoGenerator.cs
@@ -79,7 +79,7 @@ namespace LoomNetwork.CZB
         public static void PreCloudBuildExport(UnityEngine.CloudBuild.BuildManifestObject manifest)
         {
             BuildMetaInfo buildMetaInfo = GetBuildMetaInfo();
-            buildMetaInfo.CloudBuildBuildNumber = manifest.GetValue<string>("buildNumber");
+            buildMetaInfo.CloudBuildBuildNumber = Convert.ToInt32(manifest.GetValue<string>("buildNumber"));
             buildMetaInfo.CloudBuildGitBranchName = manifest.GetValue<string>("scmBranch");
             buildMetaInfo.CloudBuildGitCommitHash = manifest.GetValue<string>("scmCommitId");
             buildMetaInfo.CloudBuildTargetName = manifest.GetValue<string>("cloudBuildTargetName");
