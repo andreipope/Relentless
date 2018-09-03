ccd0f9e59cc9758597f8dfca243ea8721744ca8c
H4ch1k0 <hachisoft@hotmail.it>
2018-09-03 12:23:47 +0200

    First Draft Load from Asset Bundle

--- Assets/Scripts/App/Managers/LoadObjectsManager.cs
+++ Assets/Scripts/App/Managers/LoadObjectsManager.cs
@@ -5,16 +5,18 @@ namespace Loom.ZombieBattleground
     public class LoadObjectsManager : IService, ILoadObjectsManager
     {
         private readonly bool _loadFromResources = true;
+        private string _assetBundleName = "testbundle.test"; //move to a const? or could be helpful to load different bundles?
+        private AssetBundle _loadedBundle;
 
         public T GetObjectByPath<T>(string path)
             where T : Object
         {
             if (_loadFromResources)
             {
-                return LoadFromResources<T>(path);
+                return LoadFromAssetBundle<T>(path);
             }
 
-            return LoadFromResources<T>(path); // ToDo change into other load type
+            return LoadFromAssetBundle<T>(path); // ToDo change into other load type
         }
 
         public T[] GetObjectsByPath<T>(string path)
@@ -34,10 +36,32 @@ namespace Loom.ZombieBattleground
 
         public void Init()
         {
+            LoadAssetBundle();
         }
 
         public void Update()
         {
+        }
+
+        private void LoadAssetBundle()
+        {
+            _loadedBundle = AssetBundle.LoadFromFile(Application.streamingAssetsPath + "/" + _assetBundleName);
+        }
+
+        private T LoadFromAssetBundle<T>(string path)
+            where T : Object
+        {
+            if (_loadedBundle == null) {
+                LoadAssetBundle();
+            }
+
+            string[] splitPath = path.Split('/');
+            string filename = splitPath[splitPath.Length - 1];
+            T asset = _loadedBundle.LoadAsset<T>(filename);
+            if (asset == null) {
+                asset = LoadFromResources<T>(path);
+            }
+            return asset;
         }
 
         private T LoadFromResources<T>(string path)
