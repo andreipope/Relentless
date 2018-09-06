902f94d54a55139b1243603e048f0b5e522e609d
Serhii Yolkin <serhii.yolkin@lostpolygon.com>
2018-09-06 23:21:44 +0200

    Force rebuild asset bundles for Cloud Build

--- Assets/Scripts/Editor/Build/CloudBuildPreBuildHandler.cs
+++ Assets/Scripts/Editor/Build/CloudBuildPreBuildHandler.cs
@@ -24,7 +24,8 @@ namespace Loom.ZombieBattleground
 
             BuildPipeline.BuildAssetBundles(
                 outputPath,
-                Utilites.GetBuildAssetBundleOptions(buildTarget),
+                Utilites.GetBuildAssetBundleOptions(buildTarget) |
+                    BuildAssetBundleOptions.ForceRebuildAssetBundle,
                 buildTarget);
         }
     }
