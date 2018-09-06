2766c43d58f2ff4d90c9c682963e8e3cc92d8b8e
Artem Shyriaiev <frostweep@gmail.com>
2018-09-06 12:18:45 +0300

    improved classes

--- Assets/Scripts/App/Controllers/BattlegroundController.cs
+++ Assets/Scripts/App/Controllers/BattlegroundController.cs
@@ -895,46 +895,4 @@ namespace Loom.ZombieBattleground
     }
 
     #endregion
-
-    public class SpecificBattlegroundInfo
-    {
-        public int CurrentTurn;
-
-        public SpecificBattlegroundOverlordInfo PlayerInfo;
-        public SpecificBattlegroundOverlordInfo OpponentInfo;
-
-        public SpecificBattlegroundInfo()
-        {
-            CurrentTurn = Constants.FirstGameTurnIndex;
-            PlayerInfo = new SpecificBattlegroundOverlordInfo();
-            OpponentInfo = new SpecificBattlegroundOverlordInfo();
-        }
-
-        public class SpecificBattlegroundOverlordInfo
-        {
-            public int Health;
-            public int MaximumHealth;
-            public int CurrentGoo;
-            public int MaximumGoo;
-
-            public List<string> CardsInHand;
-            public List<string> CardsInDeck;
-            public List<string> CardsOnBoard;
-            public List<string> CardsOnGraveyard;
-
-            public SpecificBattlegroundOverlordInfo()
-            {
-                CardsInHand = new List<string>();
-                CardsInDeck = new List<string>();
-                CardsOnBoard = new List<string>();
-                CardsOnGraveyard = new List<string>();
-
-                MaximumHealth = Constants.DefaultPlayerHp;
-                Health = MaximumHealth;
-
-                MaximumGoo = Constants.DefaultPlayerGoo;
-                Health = MaximumGoo;
-            }
-        }
-    }
 }
