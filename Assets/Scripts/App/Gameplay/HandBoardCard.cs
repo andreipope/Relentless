db7e1cfd2375f3e7bf16abbb8ed4bc1a3a9aa9c6
Stanislav Sorokin <galiastr@gmail.com>
2018-09-10 15:06:37 +0300

    fixed head at the match up/VS screen, bugs with cards in hand

--- Assets/Scripts/App/Gameplay/HandBoardCard.cs
+++ Assets/Scripts/App/Gameplay/HandBoardCard.cs
@@ -172,6 +172,8 @@ public class HandBoardCard
                     Transform.position = InitialPos;
                     Transform.eulerAngles = InitialRotation;
                     _isReturnToHand = false;
+
+                    _gameplayManager.CanDoDragActions = true;
                 });
         }
     }
