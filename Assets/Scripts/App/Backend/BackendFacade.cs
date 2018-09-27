e833303492357e6eade639cb6ad80b130c6ce8ef
Artem Shyriaiev <frostweep@gmail.com>
2018-09-27 12:36:19 +0300

    backend facade functions

--- Assets/Scripts/App/Backend/BackendFacade.cs
+++ Assets/Scripts/App/Backend/BackendFacade.cs
@@ -315,5 +315,51 @@ namespace Loom.ZombieBattleground.BackendCommunication
 
         #endregion
 
+        #region Player Actions
+
+        private const string EndTurnMethod = "EndTurn";
+
+        private const string DrawCardMethod = "DrawCard";
+
+        private const string CardAttackMethod = "CardAttack";
+
+        private const string UseCardAbilityMethod = "UseCardAbility";
+        
+        private const string UseOverlordSkillMethod = "UseOverlordSkill";
+
+        private const string MulliganMethod = "Mulligan";
+
+        public async Task SendEndTurn(string userId, EndTurnModel model)
+        {
+         //  return await Contract.StaticCallAsync<EndTurnModel>(EndTurnMethod, request);
+        }
+
+        public async Task SendDrawCard(string userId, DrawCardModel model)
+        {
+            // return await Contract.StaticCallAsync<DrawCardModel>(DrawCardMethod, request);
+        }
+
+        public async Task SendCardAttack(string userId, CardAttackModel model)
+        {
+            // return await Contract.StaticCallAsync<CardAttackModel>(CardAttackMethod, request);
+        }
+
+        public async Task SendUseCardAbility(string userId, UseCardAbilityModel model)
+        {
+            // return await Contract.StaticCallAsync<UseCardAbilityModel>(UseCardAbilityMethod, request);
+        }
+
+        public async Task SendUseOverlordSkill(string userId, UseOverlordSkillModel model)
+        {
+            // return await Contract.StaticCallAsync<UseOverlordSkillModel>(UseOverlordSkillMethod, request);
+        }
+
+        public async Task SendMulligan(string userId, MulliganModel model)
+        {
+            // return await Contract.StaticCallAsync<MulliganModel>(MulliganMethod, request);
+        }
+
+        #endregion
+
     }
 }
