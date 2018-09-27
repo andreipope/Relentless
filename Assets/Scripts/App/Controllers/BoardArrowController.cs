227511d8651bb0709b7673e076b0e458f9011774
Artem Shyriaiev <frostweep@gmail.com>
2018-09-27 12:24:06 +0300

    implemented new function to make targetting arrow from to

--- Assets/Scripts/App/Controllers/BoardArrowController.cs
+++ Assets/Scripts/App/Controllers/BoardArrowController.cs
@@ -1,7 +1,15 @@
+using System;
+using UnityEngine;
+
 namespace Loom.ZombieBattleground
 {
     public class BoardArrowController : IController
     {
+        private ITimerManager _timerManager;
+        private ILoadObjectsManager _loadObjectsManager;
+
+        private GameObject _boardArrowPrefab;
+
         public BoardArrow CurrentBoardArrow { get; set; }
 
         public bool IsBoardArrowNowInTheBattle { get; set; }
@@ -12,6 +20,10 @@ namespace Loom.ZombieBattleground
 
         public void Init()
         {
+            _timerManager = GameClient.Get<ITimerManager>();
+            _loadObjectsManager = GameClient.Get<ILoadObjectsManager>();
+
+            _boardArrowPrefab = _loadObjectsManager.GetObjectByPath<GameObject>("Prefabs/Gameplay/Arrow/AttackArrowVFX_Object");
         }
 
         public void Update()
@@ -34,6 +46,30 @@ namespace Loom.ZombieBattleground
                 CurrentBoardArrow.Dispose();
                 CurrentBoardArrow = null;
             }
+        }
+
+
+        public T DoAutoTargetingArrowFromTo<T>(Transform from, object to, float delayTillDestroyArrow = 1f,
+                                               Action action = null, bool isManuallyDoAction = false) where T : BoardArrow
+        {
+            if (isManuallyDoAction)
+            {
+                action?.Invoke();
+                return null;
+            }
+
+            T arrow = UnityEngine.Object.Instantiate(_boardArrowPrefab).AddComponent<T>();
+            arrow.Begin(from.position);
+            arrow.SetTarget(to);
+
+            _timerManager.AddTimer((x) =>
+            {
+                arrow.Dispose();
+                UnityEngine.Object.Destroy(arrow.gameObject);
+                action?.Invoke();
+            }, null, delayTillDestroyArrow);
+
+            return arrow;
         }
     }
 }
