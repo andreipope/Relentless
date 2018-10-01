ef22e3fa49ae8282566cecb3bd9a65265e23cee2
Stanislav Sorokin <galiastr@gmail.com>
2018-10-01 09:31:21 +0300

    refactored and improved pvp manager. added interface to this manager for code style

---
+++ Assets/Scripts/App/Managers/Interfaces/IPVPManagaer.cs
@@ -0,0 +1,33 @@
+
+using Loom.ZombieBattleground.Data;
+using Loom.ZombieBattleground.Protobuf;
+using System;
+
+namespace Loom.ZombieBattleground
+{
+    public interface IPVPManagaer
+    {
+        event Action OnMatchCreated;
+        event Action OnMatchingStarted;
+        event Action OnPlayerLeftGame;
+
+        event Action OnGameStarted;
+        event Action OnGameEnded;
+
+        event Action OnGetEndTurnAction;
+        event Action<PlayerActionCardPlay> OnCardPlayedAction;
+        event Action<PlayerActionCardAttack> OnCardAttackedAction;
+        event Action<PlayerActionUseOverlordSkill> OnOverlordSkillUsedAction;
+        event Action<PlayerActionUseCardAbility> OnCardAbilityUsedAction;
+        event Action<PlayerActionMulligan> OnMulliganProcessUsedAction;
+        event Action<PlayerActionDrawCard> OnDrawCardAction;
+
+        FindMatchResponse MatchResponse { get; set; }
+        GetGameStateResponse GameStateResponse { get; set; }
+
+        OpponentDeck OpponentDeck { get; set; }
+        int OpponentDeckIndex { get; set; }
+
+        bool IsCurrentPlayer();
+    }
+}
