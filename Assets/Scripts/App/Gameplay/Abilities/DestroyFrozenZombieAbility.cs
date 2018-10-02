79d358fda342876a4d578df9007a43dc87aa3fae
Artem Shyriaiev <frostweep@gmail.com>
2018-10-02 14:09:14 +0300

    implemented logic of eventing when vfx animation done. improved ability and added view ability logic

--- Assets/Scripts/App/Gameplay/Abilities/DestroyFrozenZombieAbility.cs
+++ Assets/Scripts/App/Gameplay/Abilities/DestroyFrozenZombieAbility.cs
@@ -21,32 +21,15 @@ namespace Loom.ZombieBattleground
 
             if (IsAbilityResolved)
             {
-                Vector3 targetPosition = BattlegroundController.GetBoardUnitViewByModel(TargetUnit).Transform.position;
-
-                VfxObject = Object.Instantiate(VfxObject);
-                VfxObject.transform.position = Utilites.CastVfxPosition(BattlegroundController.GetBoardUnitViewByModel(AbilityUnitOwner).Transform.position);
-                targetPosition = Utilites.CastVfxPosition(targetPosition);
-                VfxObject.transform.DOMove(targetPosition, 0.5f).OnComplete(ActionCompleted);
-                ParticleIds.Add(ParticlesController.RegisterParticleSystem(VfxObject));
+                InvokeActionTriggered();
             }
         }
 
-        private void ActionCompleted()
+        protected override void VFXAnimationEndedHandler()
         {
+            base.VFXAnimationEndedHandler();
+
             TargetUnit.Die();
-
-            Vector3 targetPosition = VfxObject.transform.position;
-
-            ClearParticles();
-
-            if (AbilityData.HasVFXType(Enumerators.VFXType.Impact))
-            {
-                VfxObject = LoadObjectsManager.GetObjectByPath<GameObject>(AbilityData.GetVFXByType(Enumerators.VFXType.Impact).Path);
-
-                VfxObject = Object.Instantiate(VfxObject);
-                VfxObject.transform.position = targetPosition;
-                ParticlesController.RegisterParticleSystem(VfxObject, true);
-            }
         }
     }
 }
