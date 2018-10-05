56001c1feceacbb0f670f8306c16f077fe547e2b
Artem Shyriaiev <frostweep@gmail.com>
2018-10-05 16:07:38 +0300

    updated cards abilities for the new pvp logic

--- Assets/Scripts/App/Gameplay/Abilities/ReturnUnitsOnBoardToOwnersDecksAbility.cs
+++ Assets/Scripts/App/Gameplay/Abilities/ReturnUnitsOnBoardToOwnersDecksAbility.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using Loom.ZombieBattleground.Common;
 using Loom.ZombieBattleground.Data;
 
@@ -14,6 +15,8 @@ namespace Loom.ZombieBattleground
         public override void Activate()
         {
             base.Activate();
+
+            AbilitiesController.ThrowUseAbilityEvent(MainWorkingCard, new List<BoardObject>(), AbilityData.AbilityType, Protobuf.AffectObjectType.Character);
 
             if (AbilityCallType != Enumerators.AbilityCallType.ENTRY)
                 return;
