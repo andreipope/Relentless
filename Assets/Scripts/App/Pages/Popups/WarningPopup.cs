ad9c33286a8425cb6030d05d4b5f80c35ec2f7d1
H4ch1k0 <hachisoft@hotmail.it>
2018-10-09 15:50:15 +0200

    Brackets change

--- Assets/Scripts/App/Pages/Popups/WarningPopup.cs
+++ Assets/Scripts/App/Pages/Popups/WarningPopup.cs
@@ -49,9 +49,7 @@ namespace Loom.ZombieBattleground
         public void Show()
         {
             if (Self != null)
-            {
                 return;
-            }
 
             Self = Object.Instantiate(
                 _loadObjectsManager.GetObjectByPath<GameObject>("Prefabs/UI/Popups/WarningPopup"));
