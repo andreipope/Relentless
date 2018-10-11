01c70e86095fdfa54beaaab18071f4563525ef5d
Artem Shyriaiev <frostweep@gmail.com>
2018-10-11 15:41:54 +0300

    implemented new ability for the guard for ztorm shield

---
+++ Assets/Scripts/App/Gameplay/Abilities/AdjacentUnitsGetGuardAbility.cs
@@ -0,0 +1,60 @@
+using Loom.ZombieBattleground.Common;
+using Loom.ZombieBattleground.Data;
+using System.Collections.Generic;
+
+namespace Loom.ZombieBattleground
+{
+    public class AdjacentUnitsGetGuardAbility : AbilityBase
+    {
+        public AdjacentUnitsGetGuardAbility(Enumerators.CardKind cardKind, AbilityData ability)
+            : base(cardKind, ability)
+        {
+        }
+
+        public override void Activate()
+        {
+            base.Activate();
+
+
+            AbilitiesController.ThrowUseAbilityEvent(MainWorkingCard, new List<BoardObject>(), AbilityData.AbilityType, Protobuf.AffectObjectType.Character);
+
+            if (AbilityCallType != Enumerators.AbilityCallType.ENTRY)
+                return;
+
+            Action();
+        }
+
+        public override void Action(object info = null)
+        {
+            base.Action(info);
+
+            int targetIndex = -1;
+            for (int i = 0; i < PlayerCallerOfAbility.BoardCards.Count; i++)
+            {
+                if (PlayerCallerOfAbility.BoardCards[i].Model == AbilityUnitOwner)
+                {
+                    targetIndex = i;
+                    break;
+                }
+            }
+
+            if (targetIndex > -1)
+            {
+                if (targetIndex - 1 > -1)
+                {
+                    TakeGuardToUnit(PlayerCallerOfAbility.BoardCards[targetIndex - 1]);
+                }
+
+                if (targetIndex + 1 < PlayerCallerOfAbility.BoardCards.Count)
+                {
+                    TakeGuardToUnit(PlayerCallerOfAbility.BoardCards[targetIndex + 1]);
+                }
+            }
+        }
+
+        private static void TakeGuardToUnit(BoardUnitView unit)
+        {
+            unit?.Model.AddBuffShield();
+        }
+    }
+}
