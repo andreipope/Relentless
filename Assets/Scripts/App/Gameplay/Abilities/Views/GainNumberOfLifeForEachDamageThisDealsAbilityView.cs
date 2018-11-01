e4c3a19f3f2be28c95dc1ca4ce166d4dc76a90ac
Eugene Grudniy <eugene_grudniy@granddevs.com>
2018-11-01 12:53:19 +0200

    updated animation sapper

---
+++ Assets/Scripts/App/Gameplay/Abilities/Views/GainNumberOfLifeForEachDamageThisDealsAbilityView.cs
@@ -0,0 +1,42 @@
+using DG.Tweening;
+using Loom.ZombieBattleground.Common;
+using UnityEngine;
+
+namespace Loom.ZombieBattleground
+{
+    public class GainNumberOfLifeForEachDamageThisDealsAbilityView : AbilityViewBase<GainNumberOfLifeForEachDamageThisDealsAbility>
+    {
+        private BattlegroundController _battlegroundController;
+
+        public GainNumberOfLifeForEachDamageThisDealsAbilityView(GainNumberOfLifeForEachDamageThisDealsAbility ability) : base(ability)
+        {
+            _battlegroundController = GameClient.Get<IGameplayManager>().GetController<BattlegroundController>();
+        }
+
+        protected override void OnAbilityAction(object info = null)
+        {
+            ActionCompleted();
+        }
+
+        private void ActionCompleted()
+        {
+            ClearParticles();
+
+            //if (Ability.AbilityData.HasVisualEffectType(Enumerators.VisualEffectType.Impact))
+            {
+                Vector3 targetPosition = Utilites.CastVfxPosition(_battlegroundController.GetBoardUnitViewByModel(Ability.AbilityUnitOwner).Transform.position);
+
+                VfxObject = LoadObjectsManager.GetObjectByPath<GameObject>("Prefabs/VFX/Skills/HealingTouchVFX");//(Ability.AbilityData.GetVisualEffectByType(Enumerators.VisualEffectType.Impact).Path);
+                CreateVfx(targetPosition, true);
+            }
+
+            Ability.InvokeVFXAnimationEnded();
+        }
+
+
+        protected override void CreateVfx(Vector3 pos, bool autoDestroy = false, float duration = 3, bool justPosition = false)
+        {
+            base.CreateVfx(pos, true, 5f);
+        }
+    }
+}
