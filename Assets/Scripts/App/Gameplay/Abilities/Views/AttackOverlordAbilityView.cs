2565b664430d32233b8c2defb8f1c31224a5e2b8
Eugene Grudniy <eugene_grudniy@granddevs.com>
2018-11-01 16:15:34 +0200

    updated ability animation zeptic and hazzard

---
+++ Assets/Scripts/App/Gameplay/Abilities/Views/AttackOverlordAbilityView.cs
@@ -0,0 +1,78 @@
+using DG.Tweening;
+using Loom.ZombieBattleground.Common;
+using System;
+using UnityEngine;
+
+namespace Loom.ZombieBattleground
+{
+    public class AttackOverlordAbilityView : AbilityViewBase<AttackOverlordAbility>
+    {
+        private BattlegroundController _battlegroundController;
+
+        public AttackOverlordAbilityView(AttackOverlordAbility ability) : base(ability)
+        {
+            _battlegroundController = GameClient.Get<IGameplayManager>().GetController<BattlegroundController>();
+        }
+
+        protected override void OnAbilityAction(object info = null)
+        {
+            if (Ability.AbilityData.HasVisualEffectType(Enumerators.VisualEffectType.Moving))
+            {
+                Vector3 targetPosition = Ability.AffectObjectType == Enumerators.AffectObjectType.Character ?
+                _battlegroundController.GetBoardUnitViewByModel(Ability.TargetUnit).Transform.position :
+                Ability.TargetPlayer.AvatarObject.transform.position;
+
+                VfxObject = LoadObjectsManager.GetObjectByPath<GameObject>(Ability.AbilityData.GetVisualEffectByType(Enumerators.VisualEffectType.Moving).Path);
+
+                VfxObject = UnityEngine.Object.Instantiate(VfxObject);
+                VfxObject.transform.position = Utilites.CastVfxPosition(_battlegroundController.GetBoardUnitViewByModel(Ability.AbilityUnitOwner).Transform.position);
+                targetPosition = Utilites.CastVfxPosition(targetPosition);
+                VfxObject.transform.DOMove(targetPosition, 0.5f).OnComplete(ActionCompleted);
+                ParticleIds.Add(ParticlesController.RegisterParticleSystem(VfxObject));
+            }
+            else
+            {
+                ActionCompleted();
+            }
+        }
+
+        private void ActionCompleted()
+        {
+            ClearParticles();
+
+            //if (Ability.AbilityData.HasVisualEffectType(Enumerators.VisualEffectType.Impact))
+            {
+                Vector3 targetPosition = Vector3.zero;
+
+                VfxObject = LoadObjectsManager.GetObjectByPath<GameObject>("Prefabs/VFX/toxicDamageVFX");//(Ability.AbilityData.GetVisualEffectByType(Enumerators.VisualEffectType.Impact).Path);
+
+                Player player;
+
+                foreach (Enumerators.AbilityTargetType target in Ability.TargetTypes)
+                {
+                    switch (target)
+                    {
+                        case Enumerators.AbilityTargetType.OPPONENT:
+                            player = Ability.PlayerCallerOfAbility.Equals(GameplayManager.CurrentPlayer) ?
+                            GameplayManager.OpponentPlayer : GameplayManager.CurrentPlayer;
+                            CreateVfx(Utilites.CastVfxPosition(player.AvatarObject.transform.position), true, 5f, true);
+                            break;
+                        case Enumerators.AbilityTargetType.PLAYER:
+                            CreateVfx(Utilites.CastVfxPosition(Ability.PlayerCallerOfAbility.AvatarObject.transform.position), true, 5f, true);
+                            break;
+                        default:
+                            throw new ArgumentOutOfRangeException(nameof(target), target, null);
+                    }
+                }
+            }
+
+            Ability.InvokeVFXAnimationEnded();
+        }
+
+
+        protected override void CreateVfx(Vector3 pos, bool autoDestroy = false, float duration = 3, bool justPosition = false)
+        {
+            base.CreateVfx(pos, true, 5f);
+        }
+    }
+}
