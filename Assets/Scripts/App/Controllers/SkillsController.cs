87074a8a8944027f50661937f756b6ba8e82114c
Eugene Grudniy <eugene_grudniy@granddevs.com>
2018-11-01 17:52:57 +0200

    updated overlord skill animation freeze

--- Assets/Scripts/App/Controllers/SkillsController.cs
+++ Assets/Scripts/App/Controllers/SkillsController.cs
@@ -1170,7 +1170,7 @@ namespace Loom.ZombieBattleground
 
                     _vfxController.CreateVfx(
                         _loadObjectsManager.GetObjectByPath<GameObject>("Prefabs/VFX/Skills/Freeze_ImpactVFX"),
-                        unit);
+                        unit, isIgnoreCastVfx: true);
 
                     _soundManager.PlaySound(
                         Enumerators.SoundType.OVERLORD_ABILITIES,
@@ -1186,7 +1186,7 @@ namespace Loom.ZombieBattleground
 
                     _vfxController.CreateVfx(
                         _loadObjectsManager.GetObjectByPath<GameObject>("Prefabs/VFX/Skills/Freeze_ImpactVFX"),
-                        player);
+                        player, isIgnoreCastVfx: true);
                     _soundManager.PlaySound(
                         Enumerators.SoundType.OVERLORD_ABILITIES,
                         skill.OverlordSkill.ToString().ToLowerInvariant() + "_Impact",
