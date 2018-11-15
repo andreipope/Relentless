a28d84a1e4bc7fd4a2e51026ca3030ec4075235f
Serhii Yolkin <fearcatalyst@gmail.com>
2018-11-15 11:27:25 +0100

    Updated AOT hint Changed AOT hint auto-update logic

---
+++ Assets/Scripts/Editor/Build/AotHintFileUpdateAssetPostprocessor.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace Loom.ZombieBattleground.Editor
+{
+    public class AotHintFileUpdateAssetPostprocessor : AssetPostprocessor
+    {
+        private const string MustRegenerateAotHintKey = "ZB_MustRegenerateAotHint";
+
+        private static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths)
+        {
+            if (importedAssets.Any(s => s.EndsWith("Zb.cs")))
+            {
+                EditorPrefs.SetBool(MustRegenerateAotHintKey, true);
+            }
+        }
+
+        [UnityEditor.Callbacks.DidReloadScripts]
+        private static void OnScriptsReloaded() {
+            if (EditorPrefs.GetBool(MustRegenerateAotHintKey))
+            {
+                Debug.Log("Zb.cs changed, updating AOT hint");
+                AotHintFileUpdater.UpdateAotHint();
+                EditorPrefs.SetBool(MustRegenerateAotHintKey, false);
+            }
+        }
+    }
+}
