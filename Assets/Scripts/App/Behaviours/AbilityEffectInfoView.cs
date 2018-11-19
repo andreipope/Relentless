e0f9618493c8e2fffc7adb60a8dd9d908826eb6e
Eugene Grudniy <soapmail.com@gmail.com>
2018-11-19 16:54:40 +0200

    updated card mechanic animation reanimate

--- Assets/Scripts/App/Behaviours/AbilityEffectInfoView.cs
+++ Assets/Scripts/App/Behaviours/AbilityEffectInfoView.cs
@@ -11,5 +11,7 @@ namespace Loom.ZombieBattleground
         public float delayBeforeEffect;
         [Range(0, 20)]
         public float delayAfterEffect;
+        [Range(0, 20)]
+        public float delayForChangeState;
     }
 }
