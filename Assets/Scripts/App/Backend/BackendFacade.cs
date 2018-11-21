b576d9761e5205c97ae872934b3815e9ed4289b9
Sahin (Win) <qaraxanli.sahin@gmail.com>
2018-11-21 15:40:34 +0200

    Tests - Minor fixes

--- Assets/Scripts/App/Backend/BackendFacade.cs
+++ Assets/Scripts/App/Backend/BackendFacade.cs
@@ -323,28 +323,13 @@ namespace Loom.ZombieBattleground.BackendCommunication
 
         public async Task<RegisterPlayerPoolResponse> RegisterPlayerPool(string userId, long deckId, Address? customGameModeAddress, Google.Protobuf.Collections.RepeatedField<string> pvpTags = null)
         {
-            string tags = "";
-            if (pvpTags != null)
-            {
-                foreach (string tag in pvpTags)
-                {
-                    tags += tag.ToString ();
-                }
-            }
-            Debug.LogWarning ("PvPTags: " + tags);
-
-            if (pvpTags == null)
-            {
-                pvpTags = new Google.Protobuf.Collections.RepeatedField<string> ();
-            }
-
             RegisterPlayerPoolRequest request = new RegisterPlayerPoolRequest
             {
                 UserId = userId,
                 DeckId = deckId,
                 Version = BackendEndpoint.DataVersion,
                 RandomSeed = (long)Time.time,
-                Tags = pvpTags,
+                Tags = { },
                 CustomGame = customGameModeAddress?.ToProtobufAddress()
             };
 
