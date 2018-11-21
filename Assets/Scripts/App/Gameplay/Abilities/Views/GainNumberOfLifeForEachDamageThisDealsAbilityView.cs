c0dc0cbd4d6f6a8b869c7985ed805cb7394db8a9
Eugene Grudniy <soapmail.com@gmail.com>
2018-11-21 13:45:45 +0200

    updated card mechanic animation restore

--- Assets/Scripts/App/Gameplay/Abilities/Views/GainNumberOfLifeForEachDamageThisDealsAbilityView.cs
+++ Assets/Scripts/App/Gameplay/Abilities/Views/GainNumberOfLifeForEachDamageThisDealsAbilityView.cs
@@ -1,5 +1,6 @@
 using DG.Tweening;
 using Loom.ZombieBattleground.Common;
+using Loom.ZombieBattleground.Helpers;
 using UnityEngine;
 
 namespace Loom.ZombieBattleground
@@ -7,6 +8,8 @@ namespace Loom.ZombieBattleground
     public class GainNumberOfLifeForEachDamageThisDealsAbilityView : AbilityViewBase<GainNumberOfLifeForEachDamageThisDealsAbility>
     {
         private BattlegroundController _battlegroundController;
+
+        private string _cardName;
 
         public GainNumberOfLifeForEachDamageThisDealsAbilityView(GainNumberOfLifeForEachDamageThisDealsAbility ability) : base(ability)
         {
@@ -20,17 +23,54 @@ namespace Loom.ZombieBattleground
 
         private void ActionCompleted()
         {
-            ClearParticles();
+            _cardName = "";
+            float delayAfter = 0;
+            float delayBeforeDestroy = 5f;
+            Vector3 offset = Vector3.zero;
+            string soundName = string.Empty;
 
+            ClearParticles();
             if (Ability.AbilityData.HasVisualEffectType(Enumerators.VisualEffectType.Impact))
             {
-                Vector3 targetPosition = Utilites.CastVfxPosition(_battlegroundController.GetBoardUnitViewByModel(Ability.AbilityUnitOwner).Transform.position);
+                Vector3 targetPosition = _battlegroundController.GetBoardUnitViewByModel(Ability.AbilityUnitOwner).Transform.position;
 
                 VfxObject = LoadObjectsManager.GetObjectByPath<GameObject>(Ability.AbilityData.GetVisualEffectByType(Enumerators.VisualEffectType.Impact).Path);
-                CreateVfx(targetPosition, true, 5f);
+
+                AbilityEffectInfoView effectInfo = VfxObject.GetComponent<AbilityEffectInfoView>();
+                if (effectInfo != null)
+                {
+                    _cardName = effectInfo.cardName;
+                    delayAfter = effectInfo.delayAfterEffect;
+                    delayBeforeDestroy = effectInfo.delayBeforeEffect;
+                    offset = effectInfo.offset;
+                    soundName = effectInfo.soundName;
+                }
+
+                CreateVfx(targetPosition + offset, true, delayBeforeDestroy);
+
+                GameObject frameMaskObject = null;
+                switch (Ability.AbilityUnitOwner.InitialUnitType)
+                {
+                    case Enumerators.CardType.WALKER:
+                        frameMaskObject = VfxObject.transform.Find("WalkerMask").gameObject;
+                        break;
+                    case Enumerators.CardType.FERAL:
+                        frameMaskObject = VfxObject.transform.Find("FeralMask").gameObject;
+                        break;
+                    case Enumerators.CardType.HEAVY:
+                        frameMaskObject = VfxObject.transform.Find("HeavyMask").gameObject;
+                        break;
+                    default:
+                        break;
+                }
+
+                if (frameMaskObject != null)
+                {
+                    frameMaskObject.SetActive(true);
+                }
             }
 
-            Ability.InvokeVFXAnimationEnded();
+            InternalTools.DoActionDelayed(Ability.InvokeVFXAnimationEnded, delayAfter);
         }
 
 
