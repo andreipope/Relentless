2e30bb04c3897d55e8106d99aff8863f842292a0
Eugene Grudniy <soapmail.com@gmail.com>
2018-11-30 10:09:27 +0200

    updated general arrival animation cerberus

--- Assets/Scripts/App/Settings/Enumerators.cs
+++ Assets/Scripts/App/Settings/Enumerators.cs
@@ -694,7 +694,8 @@ namespace Loom.ZombieBattleground.Common
         {
             None,
             ShammannArrival,
-            ZVirusArrival
+            ZVirusArrival,
+            CerberusArrival
         }
 
         public enum CardNameOfAbility
