ca0ceb8deafbbddc3bfaa1716b04166b660039c2
Gaurav Garg <gaurav@loomx.io>
2018-12-07 13:16:59 +0700

    One signal prefab code

---
+++ Assets/Scripts/App/Managers/PushNotificationManager.cs
@@ -0,0 +1,21 @@
+﻿using Loom.ZombieBattleground;
+using UnityEngine;
+
+public class PushNotificationManager : IService
+{
+    public void Init()
+    {
+        ILoadObjectsManager loadObjectsManager = GameClient.Get<ILoadObjectsManager>();
+        Object.Instantiate(loadObjectsManager.GetObjectByPath<GameObject>("Prefabs/Plugin/OneSignal"));
+    }
+
+    public void Update()
+    {
+
+    }
+
+    public void Dispose()
+    {
+
+    }
+}
---
+++ Assets/Scripts/OneSignalSystem.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class OneSignalSystem : MonoBehaviour
+{
+	private const string AppId = "4bb0a2ed-ef2a-44fd-902f-0b451063e655";
+	void Start ()
+	{
+		// Enable line below to enable logging if you are having issues setting up OneSignal. (logLevel, visualLogLevel)
+        //OneSignal.SetLogLevel(OneSignal.LOG_LEVEL.INFO, OneSignal.LOG_LEVEL.INFO);
+
+		OneSignal.StartInit(AppId).HandleNotificationOpened(HandleNotificationOpened).EndInit();
+		OneSignal.inFocusDisplayType = OneSignal.OSInFocusDisplayOption.Notification;
+	}
+
+	// Gets called when the player opens the notification.
+	private static void HandleNotificationOpened(OSNotificationOpenedResult result)
+	{
+	}
+}
