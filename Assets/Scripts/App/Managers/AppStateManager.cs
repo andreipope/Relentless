2d543f827bafb0ad95fd272e506a73a7b8ebb850
H4ch1k0 <hachisoft@hotmail.it>
2019-01-21 02:45:52 +0100

    Blocked shop/pack opener again

--- Assets/Scripts/App/Managers/AppStateManager.cs
+++ Assets/Scripts/App/Managers/AppStateManager.cs
@@ -70,13 +70,11 @@ namespace Loom.ZombieBattleground
                     _uiManager.SetPage<HordeEditingPage>();
                     break;
                 case Enumerators.AppState.SHOP:
-                    _uiManager.SetPage<ShopPage>();
-                    break;
+                    _uiManager.DrawPopup<WarningPopup>($"The Shop is Disabled\nfor version {BuildMetaInfo.Instance.DisplayVersionName}\n\n Thanks for helping us make this game Awesome\n\n-Loom Team");
+                    return;
                 case Enumerators.AppState.PACK_OPENER:
-                {
-                    _uiManager.SetPage<PackOpenerPage>();
-                    break;
-                }
+                    _uiManager.DrawPopup<WarningPopup>($"The Pack Opener is Disabled\nfor version {BuildMetaInfo.Instance.DisplayVersionName}\n\n Thanks for helping us make this game Awesome\n\n-Loom Team");
+                    return;
                 case Enumerators.AppState.GAMEPLAY:
                     _uiManager.SetPage<GameplayPage>();
                     break;
