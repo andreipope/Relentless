a0dc9e5a1bf83940c5f5bafb7e9cca04569ae668
H4ch1k0 <hachisoft@hotmail.it>
2019-02-07 15:16:50 +0100

    Rebalance port update

--- Assets/Scripts/App/Settings/BackendEndpointsContainer.cs
+++ Assets/Scripts/App/Settings/BackendEndpointsContainer.cs
@@ -63,8 +63,8 @@ namespace Loom.ZombieBattleground.BackendCommunication
                     BackendPurpose.Rebalance,
                     new BackendEndpoint(
                         "https://stage-auth.loom.games",
-                        "ws://gamechain-staging.dappchains.com:46658/query",
-                        "ws://gamechain-staging.dappchains.com:46658/rpc",
+                        "ws://gamechain-staging.dappchains.com:9999/query",
+                        "ws://gamechain-staging.dappchains.com:46657/rpc",
                         "https://stage-vault.delegatecall.com/v1",
                         "v4",
                         false,
