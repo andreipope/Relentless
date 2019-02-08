e422d890f7bf3ef15991de62a8fb381529e4e42c
Serhii Yolkin <fearcatalyst@gmail.com>
2019-02-09 00:07:00 +0700

    CheckForMissingCardTests self-check

---
+++ Assets/Scripts/Tests/EditMode/SelfTests.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Loom.ZombieBattleground.BackendCommunication;
+using Loom.ZombieBattleground.Data;
+using NUnit.Framework;
+using UnityEngine.TestTools;
+
+namespace Loom.ZombieBattleground.Test
+{
+    public class SelfTests
+    {
+        [UnityTest]
+        public IEnumerator CheckForMissingCardTests()
+        {
+            List<Type> cardTestFixtureTypes = new List<Type>
+            {
+                typeof(MultiplayerTests)
+            };
+
+            List<string> testNames =
+                cardTestFixtureTypes
+                    .SelectMany(type => type.GetMethods(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public))
+                    .Where(method =>
+                        method.GetCustomAttribute<TestAttribute>() != null || method.GetCustomAttribute<UnityTestAttribute>() != null)
+                    .Select(method => method.Name.ToLowerInvariant())
+                    .ToList();
+
+            return TestUtility.AsyncTest(async () =>
+            {
+                List<Card> cardsWithMissingTests = new List<Card>();
+                MultiplayerDebugClient client = new MultiplayerDebugClient();
+                int numberOfCardsWithoutAbilities = 0;
+
+                try
+                {
+                    await client.Start(contract => new DefaultContractCallProxy(contract), enabledLogs: false);
+
+                    foreach (Card card in client.CardLibrary)
+                    {
+                        // Ignore simple cards, no point in making tests for each of them
+                        if (card.Abilities.Count == 0)
+                        {
+                            numberOfCardsWithoutAbilities++;
+                            continue;
+                        }
+
+                        if (testNames.Contains(card.Name.ToLowerInvariant()))
+                            continue;
+
+                        cardsWithMissingTests.Add(card);
+                    }
+
+                    if (cardsWithMissingTests.Count == 0)
+                        return;
+
+                    cardsWithMissingTests =
+                        cardsWithMissingTests
+                            .OrderBy(card => card.CardSetType)
+                            .ThenBy(card => card.Name.ToLowerInvariant())
+                            .ToList();
+
+                    Assert.AreEqual(
+                        0,
+                        cardsWithMissingTests.Count,
+                        $"Total {client.CardLibrary.Count} cards in library, " +
+                        $"{numberOfCardsWithoutAbilities} cards without any abilities, " +
+                        $"{client.CardLibrary.Count - numberOfCardsWithoutAbilities - cardsWithMissingTests.Count} cards have tests, " +
+                        $"{cardsWithMissingTests.Count} cards with missing tests:\n" +
+                         String.Join("\n", cardsWithMissingTests)
+                    );
+                }
+                finally
+                {
+                    await client.Reset();
+                }
+            });
+        }
+    }
+}
