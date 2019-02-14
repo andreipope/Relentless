a637a6a53f2770f0fa221f06696f5a18104cc184
Bonnie <bonnie@loomx.io>
2019-02-14 14:48:05 +0700

    Display samples of board card in MyCards page

--- Assets/Scripts/App/Pages/MyCardsPage.cs
+++ Assets/Scripts/App/Pages/MyCardsPage.cs
@@ -19,6 +19,8 @@ namespace Loom.ZombieBattleground
     public class MyCardsPage : IUIElement
     {
         private IUIManager _uiManager;
+
+        private IDataManager _dataManager;
         
         private ILoadObjectsManager _loadObjectsManager;
         
@@ -30,6 +32,13 @@ namespace Loom.ZombieBattleground
         {
             _uiManager = GameClient.Get<IUIManager>();
             _loadObjectsManager = GameClient.Get<ILoadObjectsManager>();
+            _dataManager = GameClient.Get<IDataManager>();
+            
+            CardCreaturePrefab = _loadObjectsManager.GetObjectByPath<GameObject>("Prefabs/Gameplay/Cards/CreatureCard");
+            CardItemPrefab = _loadObjectsManager.GetObjectByPath<GameObject>("Prefabs/Gameplay/Cards/ItemCard");
+            CardPlaceholdersPrefab = _loadObjectsManager.GetObjectByPath<GameObject>("Prefabs/CardPlaceholdersMyCards");
+
+            _createdBoardCards = new List<BoardCard>();
         }
 
         public void Update()
@@ -44,6 +53,14 @@ namespace Loom.ZombieBattleground
             
             _uiManager.DrawPopup<SideMenuPopup>(SideMenuPopup.MENU.MY_CARDS);
             _uiManager.DrawPopup<AreaBarPopup>();
+            
+            _highlightingVFXItem = new CardHighlightingVFXItem(Object.Instantiate(
+            _loadObjectsManager.GetObjectByPath<GameObject>("Prefabs/VFX/UI/ArmyCardSelection"), _selfPage.transform, true));
+            
+            _cardCounter = _selfPage.transform.Find("Anchor_BottomRight/Scaler/Panel_Frame/Upper_Items/Image_CardfCounter/Text_CardCounter").GetComponent<TextMeshProUGUI>();
+            
+            InitObjects();
+
         }
         
         public void Hide()
@@ -57,14 +74,131 @@ namespace Loom.ZombieBattleground
             Object.Destroy(_selfPage);
             _selfPage = null;
             
+            ResetBoardCards();
             _uiManager.HidePopup<SideMenuPopup>();
             _uiManager.HidePopup<AreaBarPopup>();
         }
         
         public void Dispose()
         {          
+            ResetBoardCards();
         }
+
+        #endregion
+
+        #region Board Cards
         
+        private TextMeshProUGUI _cardCounter;
+        
+        public List<Transform> CardPositions;
+
+        public GameObject CardCreaturePrefab, CardItemPrefab, CardPlaceholdersPrefab, CardPlaceholders;
+        
+        private List<BoardCard> _createdBoardCards;
+
+        private CardHighlightingVFXItem _highlightingVFXItem;
+        
+        private Enumerators.SetType _currentSet = Enumerators.SetType.FIRE;
+        
+        private int _currentElementPage, _numElementPages;
+        
+        private void InitObjects()
+        {
+            CardPlaceholders = Object.Instantiate(CardPlaceholdersPrefab);
+            Vector3 cardPlaceholdersPos = _selfPage.transform.Find("Anchor_BottomRight/Scaler/Panel_Content/Locator_CardPosition").position;
+            cardPlaceholdersPos.z = 0f;
+            CardPlaceholders.transform.position = cardPlaceholdersPos;
+            
+            CardPositions = new List<Transform>();
+
+            foreach (Transform placeholder in CardPlaceholders.transform)
+            {
+                CardPositions.Add(placeholder);
+            }
+
+            CalculateNumberOfPages();
+            LoadCards(0, _currentSet);
+
+            //TODO first number should be cards in collection. Collection for now equals ALL cards, once it won't,
+            //we'll have to change this.
+            _cardCounter.text = _dataManager.CachedCardsLibraryData.CardsInActiveSetsCount + "/" +
+                _dataManager.CachedCardsLibraryData.CardsInActiveSetsCount;
+        }
+
+        public void LoadCards(int page, Enumerators.SetType setType)
+        {
+            CardSet set = SetTypeUtility.GetCardSet(_dataManager, setType);
+
+            List<Card> cards = set.Cards;
+
+            int startIndex = page * CardPositions.Count;
+
+            int endIndex = Mathf.Min(startIndex + CardPositions.Count, cards.Count);
+
+            ResetBoardCards();
+            _highlightingVFXItem.ChangeState(false);
+
+            for (int i = startIndex; i < endIndex; i++)
+            {
+                if (i >= cards.Count)
+                    break;
+
+                Card card = cards[i];
+                CollectionCardData cardData = _dataManager.CachedCollectionData.GetCardData(card.Name);
+
+                // hack !!!! CHECK IT!!!
+                if (cardData == null)
+                    continue;
+
+                GameObject go;
+                BoardCard boardCard;
+                switch (card.CardKind)
+                {
+                    case Enumerators.CardKind.CREATURE:
+                        go = Object.Instantiate(CardCreaturePrefab);
+                        boardCard = new UnitBoardCard(go);
+                        break;
+                    case Enumerators.CardKind.SPELL:
+                        go = Object.Instantiate(CardItemPrefab);
+                        boardCard = new SpellBoardCard(go);
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException(nameof(card.CardKind), card.CardKind, null);
+                }
+
+                int amount = cardData.Amount;
+                boardCard.Init(card, amount);
+                boardCard.SetHighlightingEnabled(false);
+                boardCard.Transform.position = CardPositions[i % CardPositions.Count].position;
+                boardCard.Transform.localScale = Vector3.one * 0.3f;
+                boardCard.GameObject.GetComponent<SortingGroup>().sortingLayerID = SRSortingLayers.GameUI1;
+                boardCard.Transform.Find("Amount").gameObject.SetActive(false);
+
+                _createdBoardCards.Add(boardCard);
+
+                if (boardCard.LibraryCard.MouldId == _highlightingVFXItem.MouldId)
+                {
+                    _highlightingVFXItem.ChangeState(true);
+                }
+            }
+        }
+
+        private void ResetBoardCards()
+        {
+            foreach (BoardCard item in _createdBoardCards)
+            {
+                item.Dispose();
+            }
+
+            _createdBoardCards.Clear();
+        }
+
+        private void CalculateNumberOfPages()
+        {
+            _numElementPages = Mathf.CeilToInt(SetTypeUtility.GetCardSet(_dataManager, _currentSet).Cards.Count /
+                (float) CardPositions.Count);
+        }
+
         #endregion
     }
 }
