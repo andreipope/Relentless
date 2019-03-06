12d3612f7cf1b60f865da3fa4661363fcf620c93
Artem Shyriaiev <frostweep@gmail.com>
2019-03-06 12:11:28 +0200

    fixed issues with input. fixed pve stucks in gameplay fixed issue with out of bounds in card play

--- Assets/Scripts/App/Gameplay/Abilities/ReanimateAbility.cs
+++ Assets/Scripts/App/Gameplay/Abilities/ReanimateAbility.cs
@@ -94,6 +94,8 @@ namespace Loom.ZombieBattleground
             {
                 _reanimatedUnit.Model.RemoveGameMechanicDescriptionFromUnit(Enumerators.GameMechanicDescriptionType.Reanimate);
             }
+
+            _gameplayManager.CanDoDragActions = true;
         }
 
         private BoardUnitView CreateBoardUnit(WorkingCard card, Player owner)
