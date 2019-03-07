51527a5ae7eb4d52b0763da25a57dff00732d0ba
Serhii Yolkin <serhii.yolkin@gmail.com>
2019-03-07 10:55:35 +0100

    Fix

--- Assets/Scripts/App/Core/Log/Logging.cs
+++ Assets/Scripts/App/Core/Log/Logging.cs
@@ -65,7 +65,7 @@ namespace Loom.ZombieBattleground
                 if (GetLogFilePathFromEnvVar() != null)
                     return true;
 
-                return Application.isEditor;
+                return !Application.isEditor;
 #endif
             }
         }
