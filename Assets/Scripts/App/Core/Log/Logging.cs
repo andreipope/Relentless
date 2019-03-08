748f21ae3b275ae0e14cfbb889ca4efb47ad1e56
Serhii Yolkin <serhii.yolkin@gmail.com>
2019-03-08 11:03:33 +0100

    Added logging logs

--- Assets/Scripts/App/Core/Log/Logging.cs
+++ Assets/Scripts/App/Core/Log/Logging.cs
@@ -28,8 +28,12 @@ namespace Loom.ZombieBattleground
         public static ILoggerRepository GetRepository()
         {
             if (_isRepositoryCreated)
+            {
+                Debug.Log("=========== GETTING REPOSITORY");
                 return LogManager.GetRepository(RepositoryName);
+            }
 
+            Debug.Log("=========== CREATING REPOSITORY");
             _isRepositoryCreated = true;
             return LogManager.CreateRepository(RepositoryName);
         }
