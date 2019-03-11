3873d788ee061ad1b7f4768c71b4474249c06feb
Bonnie <bonnie@loomx.io>
2019-03-11 14:18:16 +0700

    Display build version info in Setting popup

---
+++ Assets/Scripts/App/Utilites/SetAppVersionTextMeshPro.cs
@@ -0,0 +1,16 @@
+﻿using Loom.ZombieBattleground.Common;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+namespace Loom.ZombieBattleground
+{
+    [RequireComponent(typeof(TextMeshProUGUI))]
+    public class SetAppVersionTextMeshPro : MonoBehaviour
+    {
+        private void Start()
+        {
+            GetComponent<TextMeshProUGUI>().text = BuildMetaInfo.Instance.FullVersionName;
+        }
+    }
+}
