19bb7651cf3a4b3aba74087e0fb4e7a9703df7ad
Bonnie <bonnie@loomx.io>
2019-03-19 17:27:49 +0700

    Update button interactable, add sound effect

--- Assets/Scripts/App/Settings/Enumerators.cs
+++ Assets/Scripts/App/Settings/Enumerators.cs
@@ -540,7 +540,12 @@ namespace Loom.ZombieBattleground.Common
             DISTRACT_LOOP,
             RAGE_LOOP,
             UNIQUE_ARRIVALS,
-            ZOMBIE_DEATH_ANIMATIONS
+            ZOMBIE_DEATH_ANIMATIONS,
+            OPEN_PACK,
+            CARD_REVEAL_MINION,
+            CARD_REVEAL_OFFICER,
+            CARD_REVEAL_GENERAL,
+            CARD_REVEAL_COMMANDER
         }
 
         public enum Stat
