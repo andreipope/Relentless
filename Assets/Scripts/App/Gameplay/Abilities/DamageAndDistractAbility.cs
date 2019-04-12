9a1ea18826b949b1dd515367e33bf128e0231054
Artem Shyriaiev <frostweep@gmail.com>
2019-04-12 18:17:12 +0300

    cleanup

--- Assets/Scripts/App/Gameplay/Abilities/DamageAndDistractAbility.cs
+++ Assets/Scripts/App/Gameplay/Abilities/DamageAndDistractAbility.cs
@@ -6,15 +6,78 @@ namespace Loom.ZombieBattleground
 {
     public class DamageAndDistractAbility : AbilityBase
     {
+        public int Damage { get; }
+
         public DamageAndDistractAbility(Enumerators.CardKind cardKind, AbilityData ability)
             : base(cardKind, ability)
         {
+            Damage = ability.Damage;
         }
 
         public override void Activate()
         {
             base.Activate();
+        }
 
+        protected override void InputEndedHandler()
+        {
+            base.InputEndedHandler();
+
+            if (IsAbilityResolved)
+            {
+                SendAction();
+                InvokeActionTriggered();
+            }
+        }
+
+        private void SendAction()
+        {
+            AbilityProcessingAction = ActionsQueueController.AddNewActionInToQueue(null, Enumerators.QueueActionType.AbilityUsageBlocker, blockQueue: true);
+
+            InvokeUseAbilityEvent(
+                new List<ParametrizedAbilityBoardObject>
+                {
+                    new ParametrizedAbilityBoardObject(TargetUnit)
+                }
+            );
+        }
+
+        public override void Action(object info = null)
+        {
+            object caller = AbilityUnitOwner != null ? AbilityUnitOwner : (object)BoardItem;
+
+            BattleController.AttackUnitByAbility(caller, AbilityData, TargetUnit, Damage);
+
+            BattlegroundController.DistractUnit(TargetUnit);
+
+            ActionsQueueController.PostGameActionReport(new PastActionsPopup.PastActionParam()
+            {
+                ActionType = Enumerators.ActionType.CardAffectingMultipleCards,
+                Caller = GetCaller(),
+                TargetEffects = new List<PastActionsPopup.TargetEffectParam>()
+                {
+                    new PastActionsPopup.TargetEffectParam()
+                    {
+                        ActionEffectType = Enumerators.ActionEffectType.ShieldDebuff,
+                        Target = TargetUnit,
+                        HasValue = true,
+                        Value = -AbilityData.Value
+                    },
+                    new PastActionsPopup.TargetEffectParam()
+                    {
+                        ActionEffectType = Enumerators.ActionEffectType.Distract,
+                        Target = TargetUnit,
+                    }
+                }
+            });
+            AbilityProcessingAction?.ForceActionDone();
+        }
+
+        protected override void VFXAnimationEndedHandler()
+        {
+            base.VFXAnimationEndedHandler();
+
+            Action();
             if (AbilityTrigger != Enumerators.AbilityTrigger.ENTRY)
                 return;
         }
