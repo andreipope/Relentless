57c247c533a6646beab58818921e6c774fb72e79
Bonnie <bonnie@loomx.io>
2019-06-17 16:37:11 +0700

    Add LocalizationUtil

---
+++ Assets/Scripts/App/Localization/LocalizationUtil.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using I2.Loc;
+
+namespace Loom.ZombieBattleground.Localization
+{
+    public static class LocalizationUtil
+    {
+        private static Dictionary<LocalizationTerm, LocalizedString> LocalizedStringDictionary = new Dictionary<LocalizationTerm, LocalizedString>();
+    
+        public static string GetLocalizedString(LocalizationTerm term)
+        {
+            if( !LocalizedStringDictionary.ContainsKey(term) )
+            {
+                LocalizedStringDictionary.Add
+                (
+                    term,
+                    new LocalizedString(term.ToString())
+                );
+            }
+            return LocalizedStringDictionary[term].ToString();
+        }
+    }
+}
