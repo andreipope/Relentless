c2cb8df8244bffcfbacbd649711ccfca231a9938
Bonnie <bonnie@loomx.io>
2019-06-17 14:00:04 +0700

    I2 Localization plugin, font assets and related settings

---
+++ Assets/Scripts/App/Localization/LocalizationTerm.cs
@@ -0,0 +1,19 @@
+﻿namespace Loom.ZombieBattleground.Localization
+{
+    public enum LocalizationTerm
+    {
+        Common_Button_Auto,
+        Common_Button_Filter,
+        Common_Button_GotIt,
+        Common_Button_Yes,
+        Common_Button_No,
+        HordeSelection_Header,
+        HordeSelection_Edit,
+        HordeSelection_Delete,
+        HordeSelection_Rename,
+        HordeSelection_Build,
+        HordeSelection_ConfirmSaveProgress,
+        HordeSelection_InfoDeleteLastDeck,
+        HordeSelection_ConfirmDeleteDeck
+    }
+}
