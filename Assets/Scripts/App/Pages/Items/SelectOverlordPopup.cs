50cfb809430630616f3b3d38543026951c072626
Gaurav Garg <gaurav@loomx.io>
2019-07-10 14:03:34 +0700

    Select overlord feedback

--- Assets/Scripts/App/Pages/Items/SelectOverlordPopup.cs
+++ Assets/Scripts/App/Pages/Items/SelectOverlordPopup.cs
@@ -211,7 +211,7 @@ namespace Loom.ZombieBattleground
             OverlordUserInstance overlord = _dataManager.CachedOverlordData.Overlords.Find(overlords => overlords.Prototype.Id == overlordId);
 
             _overlordImage.sprite = DataUtilities.GetOverlordImage(overlord.Prototype.Faction);
-            _textSelectOverlordName.text = overlord.Prototype.Name;
+            _textSelectOverlordName.text = DataUtilities.GetNickName(overlord.Prototype.Name);
             _textSelectOverlordDescription.text = overlord.Prototype.ShortDescription;
 
             Enumerators.Faction againstFaction = Constants.FactionAgainstDictionary[overlord.Prototype.Faction];
