bfdf306bca34e22449bc30eabcdd38fa7e846fb2
Bonnie <bonnie@loomx.io>
2019-08-26 17:09:34 +0700

    Localize Army page error message

--- Assets/Scripts/App/Pages/ArmyWithNavigationPage.cs
+++ Assets/Scripts/App/Pages/ArmyWithNavigationPage.cs
@@ -1,6 +1,7 @@
 using System;
 using log4net;
 using Loom.ZombieBattleground.Common;
+using Loom.ZombieBattleground.Localization;
 using OneOf;
 using OneOf.Types;
 using UnityEngine;
@@ -197,7 +198,14 @@ namespace Loom.ZombieBattleground
         private void FailAndGoToMainMenu(string customMessage = null)
         {
             _uiManager.HidePopup<LoadingOverlayPopup>();
-            _uiManager.DrawPopup<WarningPopup>(customMessage ?? "Something went wrong.\n Please try again.");
+            _uiManager.DrawPopup<WarningPopup>
+            (                
+                GameClient.Get<ILocalizationManager>().GetUITranslation
+                (
+                    LocalizationTerm.Handle_Error_General,
+                    customMessage ?? "Something went wrong.\n Please try again."
+                )
+            );
             GameClient.Get<IAppStateManager>().ChangeAppState(Enumerators.AppState.MAIN_MENU, true);
         }
     }
